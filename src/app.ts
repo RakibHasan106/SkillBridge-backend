cddb6dbc632d5ef0e53db78b6debff649d925e72
Adnan <mdrakibulhasanadnan@gmail.com>
2026-02-26 00:40:43 +0600

    Login and Registration Implemented using Better-Auth

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
 import express,{Request,Response} from "express";
+import cors from 'cors';
+import "dotenv/config";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 const app = express();
 
+app.use(cors({
+    origin: process.env.APP_URL||"https://skill-bridge-frontend-eight.vercel.app/",
+    credentials: true
+}));
+
 app.use(express.json());
+
+app.all("/api/auth/*splat",toNodeHandler(auth));
 
 app.use("/",(req:Request,res:Response)=>{
     res.status(200).json({
